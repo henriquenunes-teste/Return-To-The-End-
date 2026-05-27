0f7248d51e4f9f7221d2c758a031727d6aee8c1f
henriquenunes-teste <henrique.nunes@estudante.ifms.edu.br>
2026-05-27 11:51:44 -0400

    feat: movimentacao

--- code/src/main/java/com/code/GameplayController.java
+++ code/src/main/java/com/code/GameplayController.java
@@ -10,6 +10,10 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -24,10 +28,44 @@ public class GameplayController implements Initializable {
     @FXML
     public Label text;
     
+    @FXML
+    public ImageView player;
+    
+    @FXML
+    public AnchorPane inicial;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        text.setText(Session.session.getName());
+        inicial.sceneProperty().addListener((obs,oldScene,newScene)->{
+            if(newScene != null){
+                System.out.println(player.getLayoutY());
+                newScene.setOnKeyPressed((e)->{
+                    if(e.getCode()==KeyCode.W && player.getLayoutY()>= 0){
+                 
+                        
+                        player.setLayoutY(player.getLayoutY()-20);
+                    }
+                    
+                    if(e.getCode()==KeyCode.S && player.getLayoutY()+20<=400){
+                 
+                        
+                        player.setLayoutY(player.getLayoutY()+20);
+                    }
+                    
+                    if(e.getCode()==KeyCode.A && player.getLayoutX()>=0){
+                 
+                        
+                        player.setLayoutX(player.getLayoutX()-20);
+                    }
+                    
+                     if(e.getCode()==KeyCode.D && player.getLayoutX()+20< 600){
+                 
+                        
+                        player.setLayoutX(player.getLayoutX()+20);
+                    }
+                });
+            }
+        });
     }    
     
 }
