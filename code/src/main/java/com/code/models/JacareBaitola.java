1552b6b3a36da2114e0e2d4ccf0d143451b913c0
henriquenunes-teste <henrique.nuns@estudante.ifms.edu.br>
2026-06-17 15:11:55 -0400

    feat: JacareBaitola reinitialized

---
+++ code/src/main/java/com/code/models/JacareBaitola.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.code.models;
+
+/**
+ *
+ * @author cliente
+ */
+public class JacareBaitola extends Character implements IBoss {
+
+    public JacareBaitola(String name, Integer maxHealth, Integer level, Integer strength, Integer velocity, Integer recup, Integer durability) {
+        super(name, maxHealth, level, strength, velocity, recup, durability);
+    }
+
+    @Override
+    public void attack1(Player p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void attack2(Player p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void attack3(Player p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void attackOrder() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
