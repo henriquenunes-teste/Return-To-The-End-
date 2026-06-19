1f50d3e653177f6224ddef36bf58f807003e27bc
allanguimaraes-hash <allan.guimaraes@estudante.ifms.edu.br>
2026-06-19 09:13:03 -0400

    Código da logica, não dê merge

---
+++ code/code/src/main/java/com/code/models/Character.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.code.models;
+
+/**
+ *
+ * @author cliente
+ */
+public class Character {
+    protected Integer maxHealth;
+    private Integer health;
+    private Integer level;
+    protected Integer strength;
+    protected Integer velocity;
+    protected Integer recup;
+    protected Integer durability;
+    private String name;
+
+    public Character(String name, Integer maxHealth, Integer level, Integer strength, Integer velocity, Integer recup, Integer durability) {
+        this.name = name;
+        this.health = maxHealth;
+        this.maxHealth = maxHealth;
+        this.level = level;
+        this.strength = strength;
+        this.velocity = velocity;
+        this.recup = recup;
+        this.durability = durability;
+    }
+
+    public Integer getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(Integer maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public void setHealth(Integer health) {
+        this.health = health;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
---
+++ code/code/src/main/java/com/code/models/IBoss.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.code.models;
+
+/**
+ *
+ * @author cliente
+ */
+public interface IBoss {
+    void attack1(Player p);
+    void attack2(Player p);
+    void attack3(Player p);
+    void attackOrder();
+}
