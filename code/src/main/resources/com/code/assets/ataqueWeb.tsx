13d976e60d50d039745ffbc60a371aac05b04061
allanguimaraes-hash <allan.guimaraes@estudante.ifms.edu.br>
2026-06-27 19:39:02 -0400

    feat: adiciona sistema de inventário e upgrades
    - adiciona pontosDisponiveis e adicionarPontos() em Upgrade
    - adiciona isDead() e corrige takeDamage() em Character
    - detecta morte do boss e redireciona para tela de upgrade em BossBattle1Controller
    - implementa UpgradeController com distribuição de pontos nos 5 atributos
    - cria upgrade.fxml com botões + e - por atributo

---
+++ code/src/main/resources/com/code/assets/ataqueWeb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ataqueWeb" tilewidth="128" tileheight="128" tilecount="9" columns="3">
+ <image source="ataqueWeb.png" trans="010100" width="384" height="384"/>
+</tileset>
---
+++ code/src/main/resources/com/code/assets/cristal2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="cristal2" tilewidth="77" tileheight="63" tilecount="1" columns="1">
+ <image source="cristal2.png" trans="010100" width="77" height="63"/>
+</tileset>
